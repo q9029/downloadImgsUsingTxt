06a7582ef1723e32107e696b5aacbb95a73b118c
Haruyuki Ichino <haru.ichn@gmail.com>
2016-03-08 16:15:22 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# 画像ダウンロードスクリプト
+テキストファイルに記述されているURLから画像をダウンロードするシェルスクリプト
+
+## 使い方
+bashスクリプトを実行.  
+引数にはファイル名を入力.
+
+例:  
+`bash downloadImgsFromTxt.sh sample.txt`
+
+## テキストファイルフォーマット
+```
+http://hoge.com/XXX.jpg
+http://hoge.com/YYY.jpg
+http://hoge.com/ZZZ.jpg
+```
---
+++ downloadImgsFromTxt.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# downloadImgsFromTxt.sh
+#
+# 2016/03/08
+# Haruyuki Ichino
+#
+# Description:
+# テキストファイルに記述されているURLから画像をダウンロード
+#
+# Usage:
+#   引数) ファイル名
+#   Example:
+#   $ bash downloadImgsFromTxt.sh sample.txt
+# ---------------------------------------------------------
+
+FILENAME=./$1
+OUTPUT_DIR=output
+
+binary_path=`dirname $0`
+
+# try-catch
+set -eu
+trap catch ERR
+function catch() {
+        echo "[ERROR]Fail in "$0
+}
+
+
+# 出力ディレクトリの確認
+if [ ! -e $binary_path/$OUTPUT_DIR ]; then
+    mkdir -p $binary_path/$OUTPUT_DIR
+fi
+
+# 各URLにアクセス
+declare -i i=0
+while read line;
+do
+    echo $line
+    curl -o $binary_path/$OUTPUT_DIR/$i.jpg $line
+    i=$((i+1))
+done < $FILENAME
